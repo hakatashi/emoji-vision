78bc7475eb0f9bf444e2fc7a5208857be4d33901
Koki Takahashi <hakatasiloving@gmail.com>
2017-10-30 16:53:21 +0900

    Implement rendering of world map

--- index.babel.js
+++ index.babel.js
@@ -1,6 +1,9 @@
 require('babel-polyfill');
 
 const D3 = require('d3');
+const topojson = require('topojson');
+
+require('d3-selection-multi');
 
 process.on('unhandledRejection', (error) => {
 	throw error;
@@ -12,7 +15,7 @@ window.addEventListener('unhandledrejection', (error) => {
 
 (async () => {
 	const data = await new Promise((resolve, reject) => {
-		D3.csv('data_basics.csv', (error, data) => {
+		D3.json('https://unpkg.com/world-atlas@1/world/110m.json', (error, data) => {
 			if (error) {
 				reject(error);
 			} else {
@@ -23,44 +26,12 @@ window.addEventListener('unhandledrejection', (error) => {
 
 	const svg = D3.select('body').append('svg').attr('width', 2000).attr('height', 1000);
 
-	const circles = svg.selectAll('circle').data(data)
-		.enter().append('circle');
-
-	const bars = svg.selectAll('rect').data(data)
-		.enter().append('rect');
-
-	const texts = svg.selectAll('text').data(data)
-		.enter().append('text');
-
-	const path = svg.append('path');
-
-	circles
-		.attr('cx', (_, index) => index * 70 + 50)
-		.attr('cy', 100)
-		.attr('r', ({Value}) => Value / 2)
-		.attr('fill', ({Value}) => `rgb(${Math.floor(Value / 100 * 255)}, 0, 0)`);
-
-	bars
-		.attr('x', (_, index) => index * 70 + 35)
-		.attr('y', ({Value}) => 400 - Value * 2)
-		.attr('height', ({Value}) => Value * 2)
-		.attr('width', 30)
-		.attr('fill', ({Value}) => `rgb(${Math.floor(Value / 100 * 255)}, 0, 0)`);
-
-	texts
-		.text(({Label}) => Label)
-		.attr('x', (_, index) => (index * 70) + 50)
-		.attr('y', 30)
-		.attr('fill', 'black')
-		.attr('text-anchor', 'middle');
-
-	const line = D3.line()
-		.x((_, index) => (index * 70) + 50)
-		.y(({Value}) => 600 - Value);
-
-	path
-		.attr('d', line(data))
-		.attr('stroke-width', 3)
-		.attr('stroke', 'black')
-		.attr('fill', 'none');
+	const worldMap = topojson.feature(data, data.objects.countries);
+	const worldPath = D3.geoPath().projection(D3.geoMercator().clipExtent([[-Infinity, 0], [Infinity, 450]]));
+	const map = svg.selectAll('path').data(worldMap.features).enter().append('path').attrs({
+		d: worldPath,
+		stroke: '#BBB',
+		fill: '#666',
+		'stroke-width': 0.5,
+	});
 })();
