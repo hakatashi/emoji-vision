d979c4af92c5809905b56dd094f29b78f683e093
Koki Takahashi <hakatasiloving@gmail.com>
2017-11-07 10:48:34 +0900

    Parallelize device-tweets.py

--- device-tweets.py
+++ device-tweets.py
@@ -1,45 +1,36 @@
 import codecs
 import subprocess
 import os
+from joblib import Parallel, delayed
 
 TWEETS_DIR = './data/tweets'
 DEVICE_TWEETS_DIR = './data/selected/device-tweets'
 
+def process(month_path, device_month_path, day):
+    day_path = os.path.join(month_path, day)
+    device_output_path = os.path.join(device_month_path, day + '.json')
+    # with codecs.open(device_output_path, 'w') as f:
+    cmd = """find {} -type f -name "*.json" | xargs jq '[.[] | {{created_at, emojis, text, source}}]' | jq -s -c add > {}""".format(
+        day_path, device_output_path)
+    print("$ {}".format(cmd))
+    subprocess.call(cmd, shell=True)
 
 def main():
-    try:
-        os.mkdir(DEVICE_TWEETS_DIR)
-    except:
-        pass
-
     for year in os.listdir(TWEETS_DIR):
 
         year_path = os.path.join(TWEETS_DIR, year)
         device_year_path = os.path.join(DEVICE_TWEETS_DIR, year)
 
-        try:
-            os.mkdir(device_year_path)
-        except:
-            pass
-
         for month in os.listdir(year_path):
-
             month_path = os.path.join(year_path, month)
             device_month_path = os.path.join(device_year_path, month)
 
             try:
-                os.mkdir(device_month_path)
+                os.makedirs(device_month_path)
             except:
                 pass
 
-            for day in os.listdir(month_path):
-                day_path = os.path.join(month_path, day)
-                device_output_path = os.path.join(device_month_path, day + '.json')
-                # with codecs.open(device_output_path, 'w') as f:
-                cmd = """find {} -type f -name "*.json" | xargs jq '[.[] | {{created_at, emojis, text, source}}]' | jq -s -c add > {}""".format(
-                    day_path, device_output_path)
-                subprocess.call(cmd, shell=True)
-
+            Parallel(n_jobs=-1)([delayed(process)(month_path, device_month_path, day) for day in os.listdir(month_path)])
 
 if __name__ == '__main__':
     main()
