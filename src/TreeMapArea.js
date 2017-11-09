f56e29def0747e0d312c8c6512202eb763f2f87b
Koki Takahashi <hakatasiloving@gmail.com>
2017-11-10 01:52:35 +0900

    Display emoji inside TreeMapArea

--- src/TreeMapArea.js
+++ src/TreeMapArea.js
@@ -1,6 +1,8 @@
 const ndarray = require('ndarray');
 const D3 = require('d3');
 
+const {textwrap} = require('d3-textwrap');
+
 const WIDTH = 960;
 const HEIGHT = 500;
 const UNIT = 40;
@@ -49,22 +51,110 @@ module.exports = class TreeMapArea {
 		const node = D3.select(this.node);
 
 		const group = node.append('g').attrs({
-			transform: `translate(${x * UNIT}, ${y * UNIT})`,
+			transform: `translate(${(x + 0.5) * UNIT}, ${(y + 0.5) * UNIT})`,
 			'transform-origin': 'center',
 		});
 
-		group.append('circle').attrs({
-			cx: UNIT / 2,
-			cy: UNIT / 2,
-			r: 15,
+		const fileName = emoji.unified.startsWith('00') ? emoji.unified.slice(2).toLowerCase() : emoji.unified.toLowerCase();
+
+		const image = group.append('image').attrs({
+			class: 'emoji animated bounceIn',
+			'transform-origin': 'center',
+			'xlink:href': `node_modules/twemoji/2/svg/${fileName}.svg`,
+			width: 30,
+			height: 30,
+			x: -15,
+			y: -15,
+		}).styles({
+			cursor: 'pointer',
 		});
+
+		let isTooltipShown = false;
+		let isEraceCancelled = false;
+
+		const erase = () => {
+			image.attr('class', 'emoji animated bounceOut');
+			image.on('mouseover', null);
+			image.on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
+				this.remove(group);
+			});
+		};
+
+		image.on('mouseover', () => {
+			isTooltipShown = true;
+
+			const tooltipWidth = 200;
+			const tooltipHeight = 80;
+			const padding = 5;
+
+			const tooltipWrap = this.tooltipGroup.append('g').attrs({
+				class: 'animated fadeInDown',
+			}).styles({
+				'animation-duration': '0.2s',
+				'pointer-events': 'none',
+			});
+
+			const tooltip = tooltipWrap.append('g').attrs({
+				transform: `translate(${x}, ${y}) scale(0.6)`,
+				'transform-origin': 'center',
+			});
+
+			tooltip.append('rect').attrs({
+				x: -tooltipWidth / 2,
+				y: -tooltipHeight - 20,
+				rx: 3,
+				ry: 3,
+				width: tooltipWidth,
+				height: tooltipHeight,
+				fill: 'rgba(255, 255, 255, 0.7)',
+			});
+
+			tooltip.append('polygon').attrs({
+				points: '-5,-20 5,-20 0,-15',
+				fill: 'rgba(255, 255, 255, 0.7)',
+			});
+
+			const textGroup = tooltip.append('g').attrs({
+				fill: '#333',
+				'font-size': 8,
+				transform: `translate(${-tooltipWidth / 2 + padding}, ${-tooltipHeight - 20 + padding})`,
+			});
+
+			textGroup.append('text').text(tweet.text).call(textwrap().bounds({
+				width: tooltipWidth - padding * 2,
+				height: tooltipHeight - padding * 2,
+			}));
+
+			textGroup.selectAll('div').styles({color: '#333'});
+
+			image.on('mouseleave', () => {
+				tooltipWrap.attr('class', 'animated fadeOutUp');
+				tooltipWrap.on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
+					tooltipWrap.remove();
+				});
+				isTooltipShown = false;
+
+				if (isEraceCancelled) {
+					erase();
+				}
+			});
+		});
+
+		image.on('click', () => {
+			this.onClickEmoji(emoji);
+		});
+
+		setTimeout(() => {
+			if (isTooltipShown) {
+				isEraceCancelled = true;
+				return;
+			}
+
+			erase();
+		}, 3000);
 
 		this.currentView.set(x, y, group);
 		this.reverseMap.set(group, {x, y});
-
-		setTimeout(() => {
-			this.remove(group);
-		}, 3000);
 	}
 
 	remove(group) {
