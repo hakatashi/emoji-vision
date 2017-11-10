33b19449600d59de856f0b50a5afa40977d117cd
eiskey <modusvivendi496@gmail.com>
2017-11-10 11:48:08 +0900

    Add extraction functionality

--- lang-tweets.py
+++ lang-tweets.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 import subprocess
 
 import pandas as pd
@@ -12,33 +13,41 @@ LANG_TWEETS_DIR = './data/selected/lang-tweets'
 
 def process(month_path, lang_month_path, day):
     day_path = os.path.join(month_path, day)
-    lang_output_path = os.path.join(lang_month_path, day + '.json')
-    cmd = """find {} -type f -name "*.json" | xargs jq '[.[] | {{created_at, emojis, text, lang}}]' | jq -s -c add""".format(
-        day_path)
+    cmd = ("find {} -type f -name '*.json'"
+           " | xargs jq '[.[]"
+           " | {{created_at, emojis, lang, text}}]'"
+           " | jq -s -c add").format(day_path)
     print("$ {}".format(cmd))
 
     lang_tweets = subprocess.check_output(cmd, shell=True)
     lang_tweets = json.loads(lang_tweets)
-    lang_tweets_df = json_normalize(lang_tweets)
-    lang_tweets_df = pd.DataFrame(
-        ({'created_at': tup.created_at, 'lang': tup.lang} for tup in
-         lang_tweets_df.itertuples())
-    )
-    lang_tweets_df['created_at'] = lang_tweets_df['created_at'].apply(
-        pd.to_datetime)
-    hour_grouper = pd.Grouper(key='created_at', freq='H')
-    lang_tweets_hours = lang_tweets_df.groupby(hour_grouper)
+    lang_tweets = json_normalize(lang_tweets)
+    lang_tweets['created_at'] \
+        = lang_tweets['created_at'].apply(pd.to_datetime)
+    lang_tweets = lang_tweets.groupby(
+        pd.Grouper(key='created_at', freq='4H'))
 
-    lang_stats = []
-    for t, df in lang_tweets_hours:
-        lang_stat = df.groupby('lang').size()
-        lang_stat = lang_stat.sort_values(ascending=False)
-        lang_stat = lang_stat[:20]
-        lang_stat = json.loads(lang_stat.to_json())
-        lang_stats.append({'created_at': t.timestamp(), 'lang_stat': lang_stat})
+    top_langs = []
+    top_lang_tweets = pd.DataFrame(
+        columns=['created_at', 'emojis', 'lang', 'text'])
+    for t, df in lang_tweets:
+        stat = df.groupby('lang').size()
+        stat = stat.sort_values(ascending=False)[:20]
+        top_langs.append({'created_at': t.timestamp(),
+                          'lang': stat.to_dict()})
 
+        top_lang_tweets = top_lang_tweets.append(
+            df[df['lang'].apply(lambda x: x in stat)], ignore_index=True)
+
+    top_lang_tweets = json.loads(
+        top_lang_tweets.to_json(orient='records', force_ascii=False))
+    sampled_top_lang_tweets = [top_lang_tweets[i] for i in sorted(
+        random.sample(range(len(top_lang_tweets)), 1500))]
+
+    lang_output_path = os.path.join(lang_month_path, day + '.json')
     with open(lang_output_path, 'w') as f:
-        json.dump({'tweets': lang_tweets[::100], 'stats': lang_stats}, f)
+        json.dump({'tweets': sampled_top_lang_tweets, 'stats': top_langs},
+                  f, ensure_ascii=False, separators=(',', ':'))
 
 
 def main():
