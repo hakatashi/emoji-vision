c1778f574d12e2a74301c52f6f58e637ab3d3c09
eiskey <modusvivendi496@gmail.com>
2017-11-12 04:27:31 +0900

    Add script to create codepoint -> (name, group, subgroup) dict

--- src/EmojiDetail.jsx
+++ src/EmojiDetail.jsx
@@ -1,16 +1,19 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 const Close = require('react-icons/lib/io/close');
+import emojiCodepoints from '../data/emoji_codepoints.json';
+
+const EmojiStat = require('./EmojiStat');
 
 module.exports = class EmojiStat extends React.Component {
 	static propTypes = {
 		emoji: PropTypes.string,
 		onClickClose: PropTypes.func.isRequired,
-	}
+	};
 
 	static defaultProps = {
 		emoji: null,
-	}
+	};
 
 	constructor(state, props) {
 		super(state, props);
@@ -28,6 +31,7 @@ module.exports = class EmojiStat extends React.Component {
 				<div className="content">
 					<div className="basic-info">
 						<img src={`node_modules/twemoji/2/svg/${this.props.emoji.toLowerCase()}.svg`}/>
+						{emojiCodepoints[this.props.emoji].name} (/{emojiCodepoints[this.props.emoji].group}/{emojiCodepoints[this.props.emoji].subgroup})
 						<div className="basic-stat exo-2">
 							Tree
 						</div>
